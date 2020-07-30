04ce6d36849d421b73c69fd1574dfdec5f824156
Tomas Perez <tomasitopg6@gmail.com>
2020-07-30 17:41:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -27,6 +27,6 @@ INSERT INTO projects VALUES (4, "Finding the Secrets of Life", "Philosophy", 69,
 INSERT INTO projects VALUES (5, "Why are Conservatives and Liberals Different?", "Politics", 4000000, "10/05/2020", "11/04/2020");
 INSERT INTO projects VALUES (6, "What Makes People Give Up?", "Psychology", 11000, "12/25/2020", "01/01/2021");
 INSERT INTO projects VALUES (7, "Why Dogs are So Silly and Cute", "Biology", 1000000000, "01/01/2021", "05/15/2021");
-INSERT INTO projects VALUES (8, "Bird Research", "Biology", 1000000, "07/30/2020", "08/30/2020");
+INSERT INTO projects VALUES (8, "Why Coke Makes Mentos Go Crazy", "Chemistry", 10000, "07/30/2020", "08/30/2020");
 INSERT INTO projects VALUES (9, "Bird Research", "Biology", 1000000, "07/30/2020", "08/30/2020");
 INSERT INTO projects VALUES (10, "Bird Research", "Biology", 1000000, "07/30/2020", "08/30/2020");
